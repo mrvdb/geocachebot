7e0f9c23e467ef9817c2468f2f065882e4b2870c
Marcel van der Boom <marcel@hsdev.com>
2015-08-12 14:22:26 +0200

    Typos and README improvment

--- templates/help.cmd
+++ templates/help.cmd
@@ -1,7 +1,7 @@
 @geocachebot aims to assist with geocaching
 
 Features:
-  - scans for GC-codes and TB-code  in messages and displays info on the cache or trackable when it is valid; This works automatically, you do not need to issue a command for this. Having something like 'GC1234' in your message is enough. 
+  - scans for GC-codes and TB-codes  in messages and displays info on the cache or trackable when it is valid; This works automatically, you do not need to issue a command for this. Having something like 'GC1234' in your message is enough. 
   
 
 Commands:
@@ -10,4 +10,4 @@ Commands:
 
 Notes:
   - As the bot scans for GC codes it needs access to all messages (privacy-mode is off)
-  - Trackable information (on TB codes) can only be shown when the both is logged in to geocaching.com.
+  - Trackable information (on TB codes) can only be shown when the bot is logged in to geocaching.com.
